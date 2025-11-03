f78fe76c5d6233de265d1a62523c8679a599d2e3
MattiaPeru <peruzzimattia63@gmail.com>
2025-11-03 08:51:33 +0100

    added riverpod dependencies

--- app_code/lib/providers/supermarket_notifier.dart
+++ app_code/lib/providers/supermarket_notifier.dart
@@ -1,3 +1,6 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:app_code/models/supermarket.dart';
+
 class SupermarketNotifier extends StateNotifier<Supermarket> {
     
 }
