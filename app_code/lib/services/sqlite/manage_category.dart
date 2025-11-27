46eb906d8c37f44b76465cd8aae070e1bbe3e393
MattiaPeru <peruzzimattia63@gmail.com>
2025-11-27 09:47:24 +0100

    added firebase and sqlite folders and files to synchronize the tables

---
+++ app_code/lib/services/sqlite/manage_category.dart
@@ -0,0 +1,23 @@
+
+
+import 'package:app_code/models/category.dart';
+
+class ManageCategory {
+  void addCategory(Category category) {
+    // Code to add a category to the database
+  }
+
+  void deleteCategory(Category category) {
+    // Code to delete a category from the database
+  }
+
+  void updateCategory(Category category) {
+    // Code to update a category in the database
+  }
+
+  List<Category> getAllCategories() {
+    // Code to retrieve all categories from the database
+    return [];
+  }
+
+}
