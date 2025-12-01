387ccd446213a40eb50ea8e1c71b8e566fb0f77f
MattiaPeru <peruzzimattia63@gmail.com>
2025-12-01 10:09:14 +0100

    added database helper and manage shopping list methods signature

---
+++ app_code/lib/services/database/sqlite/manage_shopping_list.dart
@@ -0,0 +1,20 @@
+import 'package:app_code/models/shopping_list.dart';
+
+class ManageShoppingList {
+  void addShoppingList(ShoppingList category) {
+    // Code to add a shopping list to the database
+  }
+
+  void deleteShoppingList(ShoppingList category) {
+    // Code to delete a shopping list from the database
+  }
+
+  void updateShoppingList(ShoppingList category) {
+    // Code to update a shopping list in the database
+  }
+
+  List<ShoppingList> getAllShoppingLists() {
+    // Code to retrieve all shopping lists from the database
+    return [];
+  }
+}
