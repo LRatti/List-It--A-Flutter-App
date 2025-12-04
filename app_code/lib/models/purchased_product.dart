206910c4b6f2bd443de6fb545a58bcb73f9e9c11
MattiaPeru <peruzzimattia63@gmail.com>
2025-12-04 13:44:47 +0100

    added class Helper with method to generate a casual id. The id is assigned in the model classes

--- app_code/lib/models/purchased_product.dart
+++ app_code/lib/models/purchased_product.dart
@@ -1,5 +1,6 @@
 import 'package:app_code/models/product.dart';
 import 'package:isar/isar.dart';
+import 'package:app_code/utils/helper.dart';
 
 @collection
 class PurchasedProduct {
@@ -10,12 +11,12 @@ class PurchasedProduct {
   final Product product;
 
   PurchasedProduct({
-    required this.id,
+    String? id,
     required this.listId,
     required this.product,
     this.price = 0.0,
     this.quantity = 0,
-  });
+  }) : this.id = id ?? Helper.generateId();
 
   factory PurchasedProduct.fromJson(Map<String, dynamic> json) {
     return PurchasedProduct(
