54e69b80844866f3cfb272727a60ec714477ba3b
Leonardo Ratti <leonardo.ratti.mail@gmail.com>
2025-12-13 20:44:54 +0100

    Removed unsed import

--- app_code/lib/services/database/firebase/manage_shopping_list.dart
+++ app_code/lib/services/database/firebase/manage_shopping_list.dart
@@ -4,7 +4,6 @@ import 'package:app_code/models/supermarket.dart';
 import 'package:app_code/services/database/firebase/manage_purchased_product.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart' as firebase_auth;
-import 'package:firebase_core/firebase_core.dart';
 
 class FirebaseShoppingListManager {
   // Class implementation
